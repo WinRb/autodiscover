1c96b75b6b22dd5fe6b45277e1a34c936bf6fb4a
Peter Cai <222655+pcai@users.noreply.github.com>
2026-09-14 14:04:32 -0400

    Reimplement Autodiscover.logger on stdlib Logger

--- lib/autodiscover.rb
+++ lib/autodiscover.rb
@@ -8,14 +8,15 @@ require 'logger' unless defined?(Logger)
 
 # Ruby client for Microsoft's Autodiscover Service.
 module Autodiscover
-  Logging.logger['Autodiscover'].level = :info
+  @logger = Logger.new(STDOUT)
+  @logger.level = Logger::INFO
 
   def self.logger
-    Logging.logger['Autodiscover']
+    @logger
   end
 
   def logger
-    @logger ||= Logging.logger[self.class.name]
+    @logger ||= Autodiscover.logger
   end
 end
 
